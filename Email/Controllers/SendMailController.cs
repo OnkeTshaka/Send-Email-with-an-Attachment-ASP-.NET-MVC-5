80bfc7f342bee19d91e85cacb99b1f4c3c1fbf34
Onke Tshaka <21819755@dut4life.ac.za>
2020-11-27 13:04:14 +0200

    Add project files.

---
+++ Email/Controllers/SendMailController.cs
@@ -0,0 +1,61 @@
+﻿using Email.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Email.Controllers
+{
+    public class SendMailController : Controller
+    {
+        // GET: SendMail
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Thanks()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(MailModel _objModelMail, HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress("Enter your email address", "Enter company name");
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                if (upload != null)
+                {
+
+                    string filename = Path.GetFileName(upload.FileName);
+                    mail.Attachments.Add(new Attachment(upload.InputStream, filename));
+                }
+
+
+                mail.IsBodyHtml = true;
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential
+
+                ("Enter your email address", "Enter your password");
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Thanks");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Email/Models/MailModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Email.Models
+{
+    public class MailModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Body { get; set; }
+    }
+}
